8ec632e4d7ba5486d555b137cb1c2f0d8f209431
Kyle Pratuch & Jordan Johansen <student@epicodus.com>
2015-08-03 16:25:28 -0700

    Created parcel class.

---
+++ parcel.php
@@ -0,0 +1,66 @@
+<?php
+    class Parcel
+    {
+        private $height;
+        private $length;
+        private $width;
+        private $weight;
+
+
+        function __construct($pkg_height, $pkg_length, $pkg_width, $pkg_weight)
+        {
+            $this->height = $pkg_height;
+            $this->length = $pkg_length;
+            $this->width = $pkg_width;
+            $this->weight = $pkg_weight;
+        }
+
+        function setHeight($new_height)
+        {
+            $this->height = $new_height;
+        }
+        function setLength($new_length)
+        {
+            $this->length = $new_length;
+        }
+        function setWidth($new_width)
+        {
+            $this->width = $new_width;
+        }
+        function setWeight($new_weight)
+        {
+            $this->weight = $new_weight;
+        }
+        function getHeight()
+        {
+            return $this->height;
+        }
+        function getLength()
+        {
+            return $this->length;
+        }
+        function getWidth()
+        {
+            return $this->width;
+        }
+        function getWeight()
+        {
+            return $this->weight;
+        }
+
+    }
+
+    $first_parcel = new Parcel("10in", "12in", "13in", "20lbs");
+
+    $parcels = array($first_parcel);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Parcel Party!</title>
+</head>
+<body>
+    <h2><?php var_dump($parcels); ?></h2>
+</body>
+</html>
